099097e4a4121dfd04edf8e6cb60bf2923bd1688
Danil Hendra Suryawan <danilhendrasr@gmail.com>
2024-06-27 05:56:30 +0700

    feat: bootstrap nextjs

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <h1>This site is under construction.</h1>
+    </main>
+  );
+}
